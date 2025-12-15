2d7f03f961fe2ce2249cb3f6e8f5e31a53dc6fca
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2025-12-15 12:39:31 +0530

    Removed redundant text and styling from the introduction component.

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -7,28 +7,6 @@ export const Introduction = () => {
         <h1 className="text-7xl laptop:text-9xl MinimalMono mb-4 text-textPrimary font-bold text-center laptop:text-left">
           0xFF
         </h1>
-        <p className="text-lg laptop:text-xl font-[family-name:var(--font-geist-sans)] text-textPrimary flex flex-wrap items-center laptop:items-start justify-center laptop:justify-start gap-2">
-          <span className="bg-textPrimary text-white px-2 py-1 rounded-lg">
-            ⌘
-          </span>
-          <span>+</span>
-          <span className="bg-textPrimary text-white px-2.5 py-1 rounded-lg">
-            C
-          </span>
-          <span className="mx-2">and</span>
-          <span className="bg-textPrimary text-white px-2 py-1 rounded-lg">
-            ⌘
-          </span>
-          <span>+</span>
-          <span className="bg-textPrimary text-white px-2.5 py-1 rounded-lg">
-            V
-          </span>
-          <span className="ml-2">your next</span>
-          <span>component.</span>
-        </p>
-        <p className="text-lg laptop:text-xl font-[family-name:var(--font-geist-mono)] text-textPrimary text-center laptop:text-left italic pt-3">
-          Adding one component per week.
-        </p>
       </div>
       <div className="absolute inset-x-0 bottom-0 h-[70%]">
         <PerspectiveGrid />
